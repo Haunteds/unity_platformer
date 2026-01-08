4819c2236278382e54489261a9fd44eba5c8e7e8
Tuukka Tikkakoski <tuukkatikkakoski@gmail.com>
2026-01-08 19:41:23 +0200

    Added jumping for the player and fixed the rotation of the character to not spin while moving

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,10 +4,16 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
 
-    public float speed = 2f;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float jumpPower = 2f;
 
     private Rigidbody2D _rigidbody;
     private Vector2 moveInput;
+
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Vector2 capsuleSize = new Vector2(0.5f, 0.2f);
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,9 +26,26 @@ public class Movement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocityX, jumpPower);
+        }
+    }
+
     private void FixedUpdate()
     {
-        _rigidbody.linearVelocity = moveInput * speed;
+        _rigidbody.linearVelocity = new Vector2(moveInput.x * speed, _rigidbody.linearVelocityY);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position,
+            capsuleSize,
+            CapsuleDirection2D.Horizontal,
+            0f,
+            groundLayer);
     }
 
 }
